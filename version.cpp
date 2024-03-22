e464a6e9df77ad1c978556f1602be0373b4deb72
markM2864 <mrrmrk@outlook.com>
2024-03-22 15:48:25 -0400

    Remove version file

--- version.cpp
+++
@@ -1,4 +0,0 @@
-#include "/home/mark/git/ExampleCmakeandCPack/include/version.h"
-
-const char *gGIT_VERSION = "-128-NOTFOUND";
-const char *gGIT_VERSION_SHORT = "-128-NOTFOUND.-128-NOTFOUND.-128-NOTFOUND";
